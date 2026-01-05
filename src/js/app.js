b0c344c601a86337ee1cf203373e0dbe593c9686
avroratomsk <avroratomsk70@yandex.ru>
2026-01-05 13:41:38 +0300

    dev block

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,23 @@ import * as functions from './modules/baseFunctions.js';
 functions.isWebp();
 
 import "./modules/menu.js"
+
+import Swiper from "swiper";
+import {Navigation, Pagination, Scrollbar, Thumbs, EffectFade} from "swiper/modules";
+
+const worksSlider = new Swiper('.works-slider', {
+  modules: [Navigation, EffectFade],
+  direction: 'horizontal',
+  /*effect: 'fade',
+  fade: {
+    crossFade: true,
+  },*/
+  autoHeight: true,
+  slidesPerView: 3.5,
+  spaceBetween: 20,
+
+  navigation: {
+    nextEl: '.works-next',
+    prevEl: '.works-prev',
+  },
+});
